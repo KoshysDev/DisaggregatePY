245e6b198cae472397c4878aec8236f7eb868914
Koshys <koshys.osu@gmail.com>
2024-05-16 11:26:18 +0300

    compile script update

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 REM This batch file compiles main.py using PyInstaller
 
-pyinstaller --onefile --noconsole --add-data "azure.tcl;." --add-data "theme;theme" --add-data "config.py;." GUI_DIS.py
+pyinstaller --onefile --noconsole --add-data "azure.tcl;." --add-data "theme;theme" --add-data "config.py;." main.py
 
 pause
